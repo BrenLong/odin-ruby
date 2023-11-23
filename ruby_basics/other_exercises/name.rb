9ca60602da489f7d150feefa2f9ab864b00f9fc0
BrenLong <brendan.long@shopify.com>
2023-11-23 09:49:06 +0000

    just testing

---
+++ ruby_basics/other_exercises/name.rb
@@ -0,0 +1,8 @@
+# Prompt the user for their name
+print "Please enter your name: "
+
+# Read the user's input
+name = gets.chomp
+
+# Print the greeting message with the user's name
+puts "Hello, #{name}! Nice to meet you."
